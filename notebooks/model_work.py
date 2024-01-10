1e614eb32afb043b54b90e86b6c46f624251540e
MikkySupreme <MikkySupreme>
2024-01-10 09:17:57 +0000

    new model

--- notebooks/model_work.py
+++ notebooks/model_work.py
@@ -1,17 +1,24 @@
 # Databricks notebook source
+# DBTITLE 1,Chargement du modele metier
 import mlflow
 from pyspark.sql.functions import struct, col
-logged_model = 'runs:/1f27173e39b54bf48ad3ceb166812e19/model'
+logged_model = 'runs:/fe223513d81f4ff98eb93ebee0efb270/model'
 
 # Load model as a Spark UDF. Override result_type if the model does not return double values.
-loaded_model = mlflow.pyfunc.spark_udf(spark, model_uri=logged_model, result_type='double', env_manager="conda")
-# Predict on a Spark DataFrame.
-df = spark.read.table("hive_metastore.val.raw_data")
-df.withColumn('Etiquette_DPE', loaded_model(struct(*map(col, df.columns))))
+loaded_model = mlflow.pyfunc.spark_udf(spark, model_uri=logged_model, result_type='double')
 
 # COMMAND ----------
 
-df_selected = df.select("N_DPE", "Etiquette_DPE")
+df = spark.read.table("hive_metastore.train.raw_data")
+
+# COMMAND ----------
+
+# DBTITLE 1,Prediction
+df.withColumn('predictions', loaded_model(struct(*map(col, df.columns))))
+
+# COMMAND ----------
+
+df_selected = df.select("N°DPE", "Etiquette_DPE")
 path = "/mnt/mount-storage/validation.csv"
 df_selected.coalesce(1).write.csv(path, mode="append", header=True)
 
